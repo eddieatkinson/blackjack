b14a1f6dbad69916f83a39e813953034a01b1ab0
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-10 09:09:18 -0400

    Split

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,21 @@ $(document).ready(()=>{
 	const freshDeck = createDeck();
 	var theDeck = [];
 	var playersHand = [];
+	var playersHandSplit = [];
 	var dealersHand = [];
 	var wagerTotal = 0;
 	var initialMoney = 100;
 	var doubleDown = true;
+	var isSplit = false;
 	$('.deal-button').prop('disabled', true); // Make sure you place bet before dealing.
 	$('.hit-button').prop('disabled', true);
 	$('.stand-button').prop('disabled', true);
-	$('.double-down').hide();
+	$('.double-down').hide(); // We don't want to see it before the game starts.
+	$('.split').hide();
+	$('.split-message').hide();
+	$('.split-player-cards').hide();
+	$('.split-btn').hide();
+	
 
 	$('.one-dollar').click(()=>{
 		wagerTotal = increaseWager(wagerTotal, 1);
@@ -29,9 +36,10 @@ $(document).ready(()=>{
 		canIPlay();
 		console.log(wagerTotal);
 	})
+	
 
 	function canIPlay(){ // Have you met the minimum bet?
-		if(wagerTotal < 5){
+		if(wagerTotal < 5 || initialMoney <=0){
 			$('.deal-button').prop('disabled', true);
 			$('.hit-button').prop('disabled', true);
 			$('.stand-button').prop('disabled', true);
@@ -41,7 +49,7 @@ $(document).ready(()=>{
 	}
 
 	$('.deal-button').click(()=>{
-		$('.double-down').show();
+		$('.double-down').show(); // Now we can double down.
 
 		$('.message').html('Welcome to BlackJack! Please place your initial bets, with a minimum bet of 5 dollars. Good luck!');
 
@@ -58,7 +66,7 @@ $(document).ready(()=>{
 		theDeck = shuffleDeck(newDeck);
 
 		var topCard = theDeck.shift();
-		playersHand.push('5d');
+		playersHand.push('1d');
 		placeCard('player', 1, playersHand[0]);
 
 		topCard = theDeck.shift();
@@ -66,7 +74,7 @@ $(document).ready(()=>{
 		placeCard('dealer', 1, dealersHand[0]);
 
 		topCard = theDeck.shift();
-		playersHand.push('5d');
+		playersHand.push('1d');
 		placeCard('player', 2, playersHand[1]);
 
 		topCard = theDeck.shift();
@@ -84,13 +92,29 @@ $(document).ready(()=>{
 			blackJack();
 		}
 
-		$('.dealer-total').hide();
+		$('.dealer-total').hide(); // We can't see the dealer's total yet.
+
+		if(playersHand[0].slice(0, -1) == playersHand[1].slice(0, -1)){
+			$('.message').html("You got two of the same card! Click on the split to split them.");
+			$('.split').show();
+		}
 
 	});
 
-	$('.double-down').click(()=>{
-		$('.deal-button').prop('disabled', true);
-		$('.stand-button').prop('disabled', true);
+	$('.split').click(()=>{
+		$('.split-player-cards').show();
+		$('.split-message').show();
+		$('.split-btn').show();
+		$('.the-buttons').hide();
+		isSplit = true;
+		increaseWager(wagerTotal, wagerTotal);
+		wagerTotal = 2 * wagerTotal;
+		split();
+	})
+
+	$('.double-down').click(()=>{ // If we click on it...
+		$('.deal-button').prop('disabled', true); // No more dealing
+		$('.stand-button').prop('disabled', true); // We must take another card
 		$('.message').html("You have chosen to double down. You must take exactly one more card (hit it!).")
 		increaseWager(wagerTotal, wagerTotal);
 		wagerTotal = 2 * wagerTotal;
@@ -104,6 +128,12 @@ $(document).ready(()=>{
 		initialMoney -= amountToIncrease;
 		var classSelector2 = ('.amount-left');
 		$(classSelector2).html(initialMoney);
+		if(initialMoney <= 0){
+			$('.deal-button').prop('disabled', true);
+			$('.hit-button').prop('disabled', true);
+			$('.stand-button').prop('disabled', true);
+			$('.message').html("You have run out of money!")
+		}
 		return currentWager;
 	}
 
@@ -138,8 +168,13 @@ $(document).ready(()=>{
 	}
 
 	function placeCard(who, where, card){
-		var classSelector = `.${who}-cards .card-${where}`;
-		$(classSelector).html(`<img src="images/cards/${card}.png" />`);
+		if(isSplit){
+			var classSelector = `.split-${who}-cards .split-card-${where}`;
+			$(classSelector).html(`<img src="images/cards/${card}.png" />`);
+		}else{
+			var classSelector = `.${who}-cards .card-${where}`;
+			$(classSelector).html(`<img src="images/cards/${card}.png" />`);
+		}
 	}
 
 	$('.hit-button').click(()=>{
@@ -228,8 +263,74 @@ $(document).ready(()=>{
 	}
 
 	function split(){
-
+		placeCard('player', 1, playersHand[1]);
+		playersHandSplit.push(playersHand.shift());
+		$('.player-cards .card-2').html("");
+		// console.log(playersHand);
+		// console.log(playersHandSplit);
 	}
+
+	$('.split-hit-button1').click(()=>{
+		isSplit = false;
+		// $('.stand-button').prop('disabled', false);
+		// $('.double-down').hide();
+		if(calculateTotal(playersHand, 'player') < 21){
+			var topCard = theDeck.shift();
+			playersHand.push(topCard);
+			placeCard('player', playersHand.length, topCard);
+			calculateTotal(playersHand, 'player');
+		}
+		if(calculateTotal(playersHand, 'player') > 21){
+			var classSelector = '.message';
+			$(classSelector).html('You have busted!');
+			lostGame();
+		}
+		if(doubleDown){
+			$('.hit-button').prop('disabled', true);
+			$('.dealer-total').show();
+			$('.hit-button').prop('disabled', true); // Player cannot hit anymore.
+			placeCard('dealer', 2, dealersHand[1]);
+			var dealerTotal = calculateTotal(dealersHand, 'dealer');
+			while(dealerTotal < 17){
+				var topCard = theDeck.shift();
+				dealersHand.push(topCard);
+				placeCard('dealer', dealersHand.length, topCard);
+				dealerTotal = calculateTotal(dealersHand, 'dealer');
+			}
+			checkWin();
+		}
+	});
+
+	$('.split-hit-button2').click(()=>{
+		isSplit = true;
+		// $('.stand-button').prop('disabled', false);
+		// $('.double-down').hide();
+		if(calculateTotal(playersHandSplit, 'split') < 21){
+			var topCard = theDeck.shift();
+			playersHandSplit.push(topCard);
+			placeCard('player', playersHandSplit.length, topCard);
+			calculateTotal(playersHandSplit, 'split');
+		}
+		if(calculateTotal(playersHand, 'split') > 21){
+			var classSelector = '.message';
+			$(classSelector).html('You have busted!');
+			lostGame();
+		}
+		if(doubleDown){
+			$('.hit-button').prop('disabled', true);
+			$('.dealer-total').show();
+			$('.hit-button').prop('disabled', true); // Player cannot hit anymore.
+			placeCard('dealer', 2, dealersHand[1]);
+			var dealerTotal = calculateTotal(dealersHand, 'dealer');
+			while(dealerTotal < 17){
+				var topCard = theDeck.shift();
+				dealersHand.push(topCard);
+				placeCard('dealer', dealersHand.length, topCard);
+				dealerTotal = calculateTotal(dealersHand, 'dealer');
+			}
+			checkWin();
+		}
+	});
 
 	function lostGame(){
 		wagerTotal = 0;
